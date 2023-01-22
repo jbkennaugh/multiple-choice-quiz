b3f92b27a1437fdd81a4d2de87af68e0920d0592
Joe Kennaugh <joebkennaugh@gmail.com>
2023-01-22 22:50:39 +0000

    made some progress on the question and score generating

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,17 +2,34 @@ let startButton = document.querySelector("#start");
 let startDiv    = document.querySelector("#start-screen");
 let questionDiv = document.querySelector("#questions");
 let endDiv      = document.querySelector("#end-screen");
+let scoreEl     = document.querySelector("#score");
+let timerEl     = document.querySelector("#timer");
+let timer, currentScore, remainingQuestions, questionNumber;
 
-startButton.addEventListener("click", function(event) {
+startButton.addEventListener("click", init);
+
+function init(){
+    timer = 60;
+    currentScore = 0;
+    questionNumber = 0;
+    timerEl.textContent = timer;
+    scoreEl.textContent = currentScore;
+
+    startQuiz();
+}
+
+function startQuiz(){
     startDiv.classList.add("hide");
     questionDiv.classList.remove("hide");
     renderQuestion(0);
-})
+}
 
 function renderQuestion(questionNumber) {
+    console.log(questionNumber)
     let theseAnswers = answers[questionNumber];
     let choices = document.querySelector("#choices");
     document.querySelector("#question-title").textContent = questions[questionNumber][0];
+    questionNumber++;
     
     for (var i = 0; i < 4; i++){
         var answer = theseAnswers[i];
@@ -24,5 +41,8 @@ function renderQuestion(questionNumber) {
         
         li.appendChild(select);
         choices.appendChild(li);
+
+        select.addEventListener("click", function(){ renderQuestion(questionNumber); });
     }
 }
+
