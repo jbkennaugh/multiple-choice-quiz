2daffcc8a51abd6f6347c2bd35399de2c3397ec6
Joe Kennaugh <joebkennaugh@gmail.com>
2023-01-24 20:20:15 +0000

    Added functionality of displaying answers and moving to next question when answer se;lected, as well as losing time when selecting a wrong answer, and displaying your score at the end.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,47 +2,79 @@ let startButton = document.querySelector("#start");
 let startDiv    = document.querySelector("#start-screen");
 let questionDiv = document.querySelector("#questions");
 let endDiv      = document.querySelector("#end-screen");
+let scoreSubmit = document.querySelector("#submit");
+let statsDiv    = document.querySelector("#current-stats");
 let scoreEl     = document.querySelector("#score");
 let timerEl     = document.querySelector("#timer");
-let timer, currentScore, remainingQuestions, questionNumber;
+let choiceList  = document.querySelector("#choice-list");
+let initialsEl  = document.querySelector("#initials");
+let timeLeft, currentScore, remainingQuestions, questionNumber;
 
 startButton.addEventListener("click", init);
+scoreSubmit.addEventListener("click", saveHighScore);
 
 function init(){
-    timer = 60;
+    timeLeft = 60;
     currentScore = 0;
     questionNumber = 0;
-    timerEl.textContent = timer;
-    scoreEl.textContent = currentScore;
+    timerEl.textContent = "Timer: "+timeLeft;
+    scoreEl.textContent = "Score: "+currentScore;
 
     startQuiz();
 }
 
+function timer(){
+    let countdown = setInterval(function() {
+        timeLeft--;
+        if(timeLeft <= 0) { 
+            timerEl.textContent = "Timer: "+"0"; 
+            endQuiz();
+        }
+        else { timerEl.textContent = "Timer: "+timeLeft; }
+    }, 1000);
+}
+
 function startQuiz(){
+    timer();
     startDiv.classList.add("hide");
     questionDiv.classList.remove("hide");
+    statsDiv.classList.remove("hide");
     renderQuestion(0);
 }
 
+function endQuiz(){
+    questionDiv.classList.add("hide");
+    statsDiv.classList.add("hide");
+    endDiv.classList.remove("hide");
+    document.querySelector("#final-score").textContent = currentScore;
+}
+
 function renderQuestion(questionNumber) {
-    console.log(questionNumber)
     let theseAnswers = answers[questionNumber];
-    let choices = document.querySelector("#choices");
+    choiceList.innerHTML = "";
     document.querySelector("#question-title").textContent = questions[questionNumber][0];
+    correctAnswerIndex = questions[questionNumber][1] - 1;
+    correctAnswer = answers[questionNumber][correctAnswerIndex];
     questionNumber++;
     
     for (var i = 0; i < 4; i++){
         var answer = theseAnswers[i];
-        var li = document.createElement("li");
-        li.textContent = answer;
-        
+        var li = document.createElement("li");        
         var select = document.createElement("button");
-        select.textContent = "Select";
+        select.textContent = answer;
         
+        choiceList.appendChild(li);
         li.appendChild(select);
-        choices.appendChild(li);
 
-        select.addEventListener("click", function(){ renderQuestion(questionNumber); });
+        select.addEventListener("click", function(){
+            //checks if this button's answer is the correct answer
+            if(this.textContent !== correctAnswer){ timeLeft -= 10; }
+            else { currentScore++; }
+            renderQuestion(questionNumber); 
+        });
     }
 }
 
+function saveHighScore(){
+    let initials = initialsEl.value;
+}
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,12 +1,12 @@
 let questions   = [
-    ["Pick the correct spelling:", 1],
-    ["What is the recommended naming method for JavaScript variables?", 2],
-    ["", 3],
-    ["", 4],
-    ["", 1]
+    ["Pick the correct spelling:",1],
+    ["What is the recommended naming method for JavaScript variables?",2],
+    ["",3],
+    ["",4],
+    ["",1]
 ];
 
 let answers     = [
-    ["console.log()", "Console.log()", "Console.Log()", "console.Log()"],
-    ["Camel Case", "Kebab Case", "Snake Case", "Pascal Case"]
+    ["console.log()","Console.log()","Console.Log()","console.Log()"],
+    ["Kebab Case","Camel Case","Snake Case","Pascal Case"]
 ];
